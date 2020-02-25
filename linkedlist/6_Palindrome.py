349b006b70ecd88b7150eafd29dc9169316bff3b
Anton Francis Jeejo <44845055+jeejo13@users.noreply.github.com>
2020-02-25 11:39:15 +0530

    Update 6_Palindrome.py

--- linkedlist/6_Palindrome.py
+++ linkedlist/6_Palindrome.py
@@ -7,56 +7,36 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.size = 0
+        self.last_node = None
 
     def insert(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.size += 1
+        if self.last_node is None:
+            self.head = Node(data)
+            self.last_node = self.head
         else:
-            current = self.head
-            while current.next:
-                current = current.next
-            current.next = new_node
-            self.size += 1
+            self.last_node.next = Node(data)
+            self.last_node = self.last_node.next
 
-    def reverse(self, start):
-        prev = None
-        current = start
-        while current:
-            nxt = current.next
-            current.next = prev
-            prev = current
-            current = nxt
-        self.head = prev
+    def get_prev_node(self, ref_node):
+        current = self.head
+        while current and current.next != ref_node:
+            current = current.next
+        return current
 
     def is_palindrome(self):
-        current = self.head
-        flag = True
-        if self.size % 2 == 0:
-            mid = self.size // 2
-        else:
-            mid = (self.size + 1) // 2
-        for i in range(1, mid):
-            current = current.next
-        reverse_head = self.reverse(current.next)
-        current = self.head
-        while current is not None and reverse_head is not None:
-            if current.data != reverse_head.data:
-                flag = False
-                break
-            current = current.next
-            reverse_head = reverse_head.next
-        if flag:
-            print('yes')
-        else:
-            print('no')
-
-
+        start = self.head
+        end = self.last_node
+        while start != end and end.next != start:
+            if start.data != end.data:
+                return False
+            start = start.next
+            end = self.get_prev_node(end)
+        return True
 x = LinkedList()
-size_of_array = int(input())
 array = input().split()
 for i in array:
     x.insert(i)
-x.is_palindrome()
+if x.is_palindrome():
+    print('yes')
+else:
+    print('no')
